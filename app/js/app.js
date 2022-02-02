3f2890a6797aea3dc60f5473a166455f803d0c24
faha1999 <kafahad1999@gmail.com>
2022-02-02 22:07:18 +0600

    complete header & search bar

---
+++ app/js/app.js
@@ -0,0 +1,25 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+  navbar.classList.add('active');
+};
+
+document.querySelector('#nav-close').onclick = () => {
+  navbar.classList.remove('active');
+};
+
+window.onscroll = () => {
+  navbar.classList.remove('active');
+};
+
+// search
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+  searchForm.classList.add('active');
+};
+
+document.querySelector('#close-search').onclick = () => {
+  searchForm.classList.remove('active');
+};
