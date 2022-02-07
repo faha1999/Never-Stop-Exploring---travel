5fb17507a1f84d1615af112c077a77caead36651
faha1999 <kafahad1999@gmail.com>
2022-02-07 22:51:13 +0600

    complete reviews

--- app/js/app.js
+++ app/js/app.js
@@ -74,3 +74,26 @@ var swiper = new Swiper('.products-slider', {
     }
   }
 });
+
+// reviews
+// shop slide
+var swiper = new Swiper('.review-slider', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    640: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 3
+    }
+  }
+});
