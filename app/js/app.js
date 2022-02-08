b57a741996f7aab4ef2e64fa04a6551e11605007
faha1999 <kafahad1999@gmail.com>
2022-02-08 16:49:34 +0600

    complete clients

--- app/js/app.js
+++ app/js/app.js
@@ -138,3 +138,29 @@ var swiper = new Swiper('.blogs-slider', {
     }
   }
 });
+
+// clients
+var swiper = new Swiper('.clients-slider', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    640: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 3
+    },
+    1024: {
+      slidesPerView: 4
+    }
+  }
+});
